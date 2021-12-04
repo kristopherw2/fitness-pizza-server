e6302222f328e42e6afce7cb0ad3d33ece77e939
Kristopher Williams <kristopherwilliams@Kristophers-Mac-mini.local>
2021-12-04 12:11:53 -0600

    fixed for heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,4 @@ module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV ==='test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
-    "ssl": !!process.env.SSL
 }
